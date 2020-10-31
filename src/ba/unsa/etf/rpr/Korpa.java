5eb9988a58e9a8f6091a7664267e743a188ba202
Armin Žunić <azunic1@etf.unsa.ba>
2020-10-31 19:11:04 +0100

    Dorađene sve klase, mislim da kod radi ono što se od njega i očekuje

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,52 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] niz;
+    private int broj_artikala;
+
+    public Korpa() {
+        niz = new Artikl[50];
+        broj_artikala = 0;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        if(broj_artikala > 49)  return false;
+        else
+        {
+            niz[broj_artikala] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            broj_artikala = broj_artikala + 1;
+            return true;
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return niz;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl pom = null;
+        for (int i = 0; i < broj_artikala; i++)
+        {
+            if(kod.equals(niz[i].getKod()))
+            {
+                pom = niz[i];
+                int j;
+                for (j = i; j < broj_artikala-1; j++)
+                {
+                    niz[j] = niz[j+1];
+                }
+                niz[j] = null;
+                broj_artikala = broj_artikala - 1;
+                break;
+            }
+        }
+        return pom;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for (int i = 0; i < broj_artikala; i++)
+            suma = suma + niz[i].getCijena();
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] niz;
+    private int broj_artikala;
+
+    public Supermarket() {
+        niz = new Artikl[1000];
+        broj_artikala = 0;
+    }
+
+    public void dodajArtikl(Artikl a) {
+        niz[broj_artikala] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+        broj_artikala = broj_artikala + 1;
+    }
+
+    public Artikl[] getArtikli() {
+        return niz;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl pom = null;
+        for (int i = 0; i < broj_artikala; i++)
+        {
+            if(kod.equals(niz[i].getKod()))
+            {
+                pom = niz[i];
+                int j;
+                for (j = i; j < broj_artikala-1; j++)
+                {
+                    niz[j] = niz[j+1];
+                }
+                niz[j] = null;
+                broj_artikala = broj_artikala - 1;
+                break;
+            }
+        }
+        return pom;
+    }
 }
